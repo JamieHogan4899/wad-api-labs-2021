9244ff658d192a04671a4ff42d3ac7502dfff46a
Jamie Hogan <20084469@mail.wit.ie>
2021-11-23 13:28:49 +0000

    Mongo lab User Favourites

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
+const MovieSchema = new Schema({
+    id: Number,
+    title: String
+  });
+  
+  const UserSchema = new Schema({
+    username: { type: String, unique: true, required: true},
+    password: {type: String, required: true },
+    favourites: [MovieSchema]
+  });
 
-const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
-});
-
+  
 export default mongoose.model('User', UserSchema);
